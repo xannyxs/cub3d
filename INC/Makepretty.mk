f995f2cf6af8938e738ad4ce486496260dea6efe
Sue <54895147+SwenneHacks@users.noreply.github.com>
2022-06-13 19:41:19 +0200

    Revert "deleted gitmodules and updated final Makefiles"
    This reverts commit f9e6a1f32a44259144831fed5d529f6efeac2bd1.

--- INC/Makepretty.mk
+++ INC/Makepretty.mk
@@ -1,42 +1,36 @@
 
-# MAKE IT PRETTY
--- 	= ---------
---- = $(--)$(--)--
-N	= \n
-
 # COLORS
+RED		= \x1b[31m
+PINK	= \x1b[35m
+BLUE	= \x1b[34m
+LOVE	= \x1b[36m
 RESET	= \x1b[0m
-RED		= \x1b[31;1m
-BLACK	= \033[30;1m
-MAGENTA	= \033[35;1m
-YELLOW	= \x1b[33;1m
-PINK	= \x1b[35;1m
-BLUE	= \x1b[34;1m
-GREEN	= \x1b[32;1m
-BOLD	= \033[1m
-NC		= \033[0m# No Color
+GREEN	= \x1b[32m
+YELLOW	= \x1b[33m
+NC		= \033[0m # No Color
 
 # COMMANDS
 RM = rm -rf
 MAKE_C = make -C
 NEW_DIR = mkdir -p
 
-START_MLX42		= @echo "$(N)$(MESSY_MESSAGE)$(GREEN)$(---)$(N)  Starting MLX42$(N)$(---)$(RESET)"
-START_CUB3D		= @echo "$(PINK)$(---)-----$(N)    Starting CUB3D$(N)$(---)-----$(N)$(RESET)"$(MESSAGE_CUB3D)
-PROJECT_MESSAGE = @echo "$(YELLOW)You can now run ./$(NAME)$(N)$(NC)"
+START_MESSAGE	= @echo "$(BLUE)---\nStarting...!\n---$(NC)"
+REM_MESSAGE		= @echo "$(RED)Removing files...$(NC)"
+RUN_MESSAGE		= @echo "$(BLUE)---\nCompiling done! Run ./$(NAME)\n---$(NC)"
+COMP_MESSAGE	= "$(GREEN)Building C object... $(NC)%-33.33s\r\n"
 
-MESSAGE_CUB3D	= "$(PINK)Compiling$(RESET) >>>> CUB3D <<<<"
+PROJECT_MESSAGE = @echo "$(YELLOW)~~~~~~~~~ Done, you can now use this Super-$(NAME) o/"
 COMPILE_MESSAGE	= @echo "$(PINK)Compiling$(RESET) $< $@"
-REM_MESSAGE		= @echo "$(RED)$(N)Cleaning $(NC)CUB3D"
-RUN_MESSAGE		= @echo "$(PINK)$(--)$(N)Compiling Done!$(N)$(--)$(N)"
+MESSAGE_LINKED	= @echo "$(GREEN)Compiling$(RESET) $< $@"
+MESSAGE_PRINTF	= @echo "$(BLUE)Compiling$(RESET) $< $@"
 
 EMPTY_MESSAGE	= @echo ""
-MESSY_MESSAGE	= . $(YELLOW)LET'S $(BLUE)MAKE $(PINK)CUB3D$(RESET) .$(N)$(N)
-DONE_MESSAGE 	= @echo "$(YELLOW)	done.$(N)"
+MESSY_MESSAGE	= @echo "$(GREEN)$@ $(PINK)$(NAME)"
+DONE_MESSAGE 	= @echo "$(YELLOW)	done.\n"
 
-RESET_MESSAGE	= @echo "	 $(RESET)Done.\n"
+RESET_MESSAGE_3	= @echo "	   $(RESET)Done"
 RESET_MESSAGE_2	= @echo "	$(PINK)RE$(GREEN)COM$(YELLOW)PI$(BLUE)LIN$(LOVE)G"
-RESET_MESSAGE_1	= @echo "	$(RESET)   $(NAME)$(N)"
+RESET_MESSAGE_1	= @echo "	$(RESET)   $(NAME)\n"
 
-ADD_GIT_IGNORE	= @echo $@ "$(N)"obj "$(N)".gitignore >> .gitignore
+ADD_GIT_IGNORE	= @echo $@ "\n"obj "\n".gitignore >> .gitignore
 BYE_GIT_IGNORE	= @$(RM) .gitignore
